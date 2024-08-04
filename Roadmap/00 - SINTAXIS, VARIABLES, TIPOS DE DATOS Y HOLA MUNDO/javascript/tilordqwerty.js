2c0edac0177e0cdde27f2d266434dc4c27a19e2e
TilordQwerty <124811524+TilordQwerty@users.noreply.github.com>
2024-08-04 16:38:32 -0400

    Create tilordqwerty.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/tilordqwerty.js
@@ -0,0 +1,58 @@
+// /*
+ /* ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+* - Recuerda que todas las instrucciones de participación están en el
+*   repositorio de GitHub.
+*
+* Lo primero... ¿Ya has elegido un lenguaje?
+* - No todos son iguales, pero sus fundamentos suelen ser comunes.
+* - Este primer reto te servirá para familiarizarte con la forma de participar
+*   enviando tus propias soluciones.
+*
+* EJERCICIO:
+* - Crea un comentario en el código y coloca la URL del sitio web oficial del
+*   lenguaje de programación que has seleccionado.
+* - Representa las diferentes sintaxis que existen de crear comentarios
+*   en el lenguaje (en una línea, varias...).
+* - Crea una variable (y una constante si el lenguaje lo soporta).
+* - Crea variables representando todos los tipos de datos primitivos
+*   del lenguaje (cadenas de texto, enteros, booleanos...).
+* - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+*
+* ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+* debemos comenzar por el principio.
+*/
+
+// "https://developer.mozilla.org/es/docs/Web/JavaScript"
+
+// Esto es 
+// un comentario 
+// en varias lineas
+
+// mis primeras variables
+ 
+myVariable = "Mi variable"
+myVariable = "Mi variable tiene un nuevo valor"
+
+// constante
+
+const MY_CONSTANT = "Mi constante"
+
+// Valores primitivos
+
+myIntVar = 1
+myFloatVar = 1.5
+myBooleanVar = true
+myBooleanVar2 = false
+myString = "Mi cadena de texto"
+myOtherString = 'Mi otra cadena de texto'
+
+// Consola
+
+console.log("hello, javascript!")
+
+console.log(typeof(myIntVar))
+console.log(typeof(myFloatVar))
+console.log(typeof(myBooleanVar))
+console.log(typeof(myBooleanVar2))
+console.log(typeof(myString))
+console.log(typeof(myOtherString))
